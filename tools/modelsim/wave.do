02a73a6d94d649f368aecbbc2448f287ea8c8e97
Alessandro Cilardo <acilardo@unina.it>
2019-07-20 17:17:28 +0200

    new kernels added

--- tools/modelsim/wave.do
+++
@@ -1,2 +0,0 @@
-# placeholder
-# put your waveform configuration here
--- tools/test.sh
+++ tools/test.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-#            Copyright 2019 NaplesPU
+#        Copyright 2019 NaplesPU
 #   
+#   	 
 #   Redistribution and use in source and binary forms, with or without modification,
 #   are permitted provided that the following conditions are met:
 #   
 #   1. Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
+#      this list of conditions and the following disclaimer.
 #   
 #   2. Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
+#      this list of conditions and the following disclaimer in the documentation
+#      and/or other materials provided with the distribution.
 #   
 #   3. Neither the name of the copyright holder nor the names of its contributors
-#   may be used to endorse or promote products derived from this software
-#   without specific prior written permission.
+#      may be used to endorse or promote products derived from this software
+#      without specific prior written permission.
 #   
-#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-#   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-#   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-#   IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-#   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-#   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-#   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-#   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-#   EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#      
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+#   IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+#   INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+#   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+#   OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ##---------------------------------
 ## Function used to describe the 
@@ -160,7 +163,7 @@ logo
 # Kernels under test list
 # lud_float has rounding errors, the output diverges from 
 # the expected one.
-KERNELS=( barriertest mmsc mmsc_float mmsc_tiling dct_scalar conv_layer_scalar_mt conv_layer_mvect_mt lud_scalar lud_float matrix_transpose marching_squares ) 
+KERNELS=( barriertest mmsc mmsc_float mmsc_tiling crc dct_scalar ns ndes fir conv_layer_scalar_mt conv_layer_mvect_mt lud_scalar lud_float matrix_transpose marching_squares ) 
 
 # Checking the NPU toolchain installation
 if [ ! -e "$MANGO_ROOT/usr/local/llvm-npu" ]; then
